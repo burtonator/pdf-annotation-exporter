e756f2df727c9c2d06fbec12d8abd7d965b9af7c
burton <burton@spinn3r.com>
2018-05-23 10:08:45 -0700

    ... fix this...

--- annotations2html.js
+++ annotations2html.js
@@ -2,7 +2,9 @@
 
 fs = require('fs')
 
-fs.readFile('example.json.txt', 'utf8', function (err,data) {
+var path = process.argv[2]
+
+fs.readFile(path, 'utf8', function (err,data) {
 
     if (err) {
         return console.log(err);
